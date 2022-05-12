b68f9244a24984f5dd7babb08d23d1b66f3e5f54
smatus9 <smatus@uc.cl>
2022-05-12 10:48:29 -0400

    deleted useless comentaries

--- config/config.js
+++ config/config.js
@@ -1,27 +1,3 @@
-// {
-//   "development": {
-//     "username": "root",
-//     "password": null,
-//     "database": "database_development",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   },
-//   "test": {
-//     "username": "root",
-//     "password": null,
-//     "database": "database_test",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   },
-//   "production": {
-//     "username": "root",
-//     "password": null,
-//     "database": "database_production",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   }
-// }
-
 const config = {
   default: {
       username: process.env.DB_USERNAME,
