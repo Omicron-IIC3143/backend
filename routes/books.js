9676da53e67f01cdc28dd0d0d514bdf26b9fce7c
smatus9 <smatus@uc.cl>
2022-05-12 14:17:39 -0400

    testing auto deploy is working

--- routes/books.js
+++ routes/books.js
@@ -9,7 +9,8 @@ let books = [
 	{ id: 101, name: 'Fight Club', author: 'Chuck Palahniuk' },
 	{ id: 102, name: 'Sharp Objects', author: 'Gillian Flynn' },
 	{ id: 103, name: 'Frankenstein', author: 'Mary Shelley' },
-	{ id: 101, name: 'Into The Wild', author: 'John Krakauer' }
+	{ id: 104, name: 'Into The Wild', author: 'John Krakauer' },
+	{ id: 105, name: 'Auto Deploy Working', author: 'Heroku' }
 ];
 
 // Routes will go here
