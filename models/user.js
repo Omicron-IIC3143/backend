d683d3501e7ef474e86753edb4cf336d3bfb0df6
smatus9 <smatus@uc.cl>
2022-05-22 17:40:41 -0400

    bug fix v1

--- models/user.js
+++ models/user.js
@@ -16,7 +16,7 @@ module.exports = (sequelize, DataTypes) => {
 		}
 	}
 	User.init({
-		name: DataTypes.STRING,
+		name: DataTypes.STRING, 
 		email: DataTypes.STRING,
 		rut: DataTypes.STRING,
 		password: DataTypes.STRING,
