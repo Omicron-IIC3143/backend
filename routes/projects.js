fb907590a38c561f292fefadf80acb1d17dc3809
smatus9 <smatus@uc.cl>
2022-05-23 11:49:37 -0400

    implementation finished

--- routes/projects.js
+++ routes/projects.js
@@ -45,4 +45,43 @@ router.post('/new', async (ctx) => {
 	}
 });
 
+router.delete('/delete/:id', async (ctx) => {
+	try {
+		var param = ctx.params.id;
+		const deleted = await ctx.db.Project.destroy({
+			where: {
+				id: param
+			},});
+		if (deleted > 0) {
+			ctx.response.status = 200;
+			ctx.body = `Project ${param} deleted`;
+		}
+		else{
+			throw new Error('Project not found');
+		}
+	} catch (ValidationError) {
+		ctx.throw(400, `The parameters you have given are not valid, here is the error ${ValidationError}`);
+	}
+});
+
+router.put('/:id', async (ctx) => {
+	// eslint-disable-next-line no-unused-vars
+	try{
+		var param = ctx.params.id;
+		var data = ctx.request.body;
+		const update = await ctx.db.Project.update(data, {
+			where: {
+				id: param
+			},});
+		if (update > 0){
+			ctx.response.status = 200;
+			ctx.body = 'Project updated';
+		}else{
+			throw new Error('Something is wrong');
+		}
+	} catch (ValidationError) {
+		ctx.throw(400, `The parameters you have given are not valid, here is the error ${ValidationError}`);
+	}
+});
+
 module.exports = router;
