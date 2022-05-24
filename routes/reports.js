2b70c97a467f3736095d7dee962e90d3d78a88f4
vicentegonz <vgonzalez4@uc.cl>
2022-05-24 14:25:57 -0400

    fix redundancies and apply try/catch to every route

--- routes/reports.js
+++ routes/reports.js
@@ -25,13 +25,12 @@ router.get('/', async (ctx, next) => {
 //Get id
 router.get('/:id', async (ctx, next) => {
 	try{
-		var param = ctx.params.id; 
 		let report = await ctx.db.Reports.findAll({
 			where: {
-				id: param
+				id: ctx.params.id
 			},});
 		if (report.length === 0){
-			throw new Error(`There's no report under id: ${param}`);
+			throw new Error(`There's no report under id: ${ctx.params.id}`);
 		}else {
 			ctx.body = report;
 			next();
@@ -57,7 +56,7 @@ router.post('/new', async (ctx) => {
 			ctx.response.status = 201;
 			ctx.body = 'New report added';
 		}else{
-			throw new Error(`There's no project under the id: ${ctx.request.body.projectId}`);
+			throw new Error('Couldn\'t add the new report');
 		}
 	} catch (ValidationError) {
 		ctx.throw(400, `${ValidationError}`);
@@ -66,14 +65,13 @@ router.post('/new', async (ctx) => {
 
 router.delete('/delete/:id', async (ctx) => {
 	try {
-		var param = ctx.params.id;
 		const deleted = await ctx.db.Reports.destroy({
 			where: {
-				id: param
+				id: ctx.params.id
 			},});
 		if (deleted > 0) {
 			ctx.response.status = 200;
-			ctx.body = `Report ${param} deleted`;
+			ctx.body = `Report ${ctx.params.id} deleted`;
 		}
 		else{
 			throw new Error('Report not found');
@@ -86,7 +84,6 @@ router.delete('/delete/:id', async (ctx) => {
 router.put('/:id', async (ctx) => {
 	// eslint-disable-next-line no-unused-vars
 	try{
-		var param = ctx.params.id;
 		var data = ctx.request.body;
 		let project = await ctx.db.Project.findAll({
 			where: {
@@ -96,7 +93,7 @@ router.put('/:id', async (ctx) => {
 		if (project.length ===1){
 			const update = await ctx.db.Reports.update(data, {
 				where: {
-					id: param
+					id: ctx.params.id
 				},});
 			if (update > 0){
 				ctx.response.status = 200;
