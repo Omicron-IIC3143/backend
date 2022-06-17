cd8569fafb9c40b4664bdd343d1d51745c991b48
smatus9 <smatus@uc.cl>
2022-06-17 18:31:05 -0400

    implemented testing for all models

---
+++ models/__tests__/funding.test.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-undef */
+const app = require('../../app');
+
+describe('Funding Model Test Suite', function() {
+  let fundingId;
+	test('create funding', async () => {
+		// Setup for this test specifically.
+		const fundingBody = {
+      userId: 2,
+			projectId: 1,
+			amount: 100.0,
+			createdAt: new Date(),
+		};
+		const testFunding = await app.context.db.Funding.create(fundingBody);
+    fundingId = testFunding.id;
+		const dbFunding = await app.context.db.Funding.findOne({
+			 where: { 
+					id: fundingId,
+			  },
+		});
+		expect(dbFunding.userId).toEqual(fundingBody.userId);
+		expect(dbFunding.projectId).toEqual(fundingBody.projectId);
+    expect(dbFunding.amount).toEqual(fundingBody.amount);
+		expect(dbFunding.createdAt).toEqual(fundingBody.createdAt);
+	});
+  test('delete funding', async () => {
+		// Setup for this test specifically.
+		await app.context.db.Funding.destroy({ 
+			where: {
+				id: fundingId,
+			}
+		 });
+		const deletedFunding = await app.context.db.Funding.findOne({
+			where: { 
+				 id: fundingId,
+			 },
+	 	});
+		expect(deletedFunding).toBeNull();
+	});
+});
---
+++ models/__tests__/project.test.js
@@ -0,0 +1,56 @@
+/* eslint-disable no-undef */
+const app = require('../../app');
+
+describe('Project Model Test Suite', function() {
+  let projectId;
+	test('create project', async () => {
+		// Setup for this test specifically.
+		const projectBody = {
+      name: 'testing project name',
+			description: 'testing project description',
+			pictureUrl: 'testing picture url',
+			company: 'testing company',
+			topic: 'testing topic',
+			currentAmount: 1000.0,
+			goalAmount: 54020.0,
+			currentState: 'pending',
+			date: new Date(),
+			tags: 'testign tag',
+			createdAt: new Date(),
+			userId: 2,
+		};
+		const testProject = await app.context.db.Project.create(projectBody);
+    projectId = testProject.id;
+		const dbProject = await app.context.db.Project.findOne({
+			 where: { 
+					id: projectId,
+			  },
+		});
+		expect(dbProject.name).toEqual(projectBody.name);
+    expect(dbProject.description).toEqual(projectBody.description);
+    expect(dbProject.pictureUrl).toEqual(projectBody.pictureUrl);
+    expect(dbProject.company).toEqual(projectBody.company);
+    expect(dbProject.topic).toEqual(projectBody.topic);
+    expect(dbProject.currentAmount).toEqual(projectBody.currentAmount);
+    expect(dbProject.goalAmount).toEqual(projectBody.goalAmount);
+    expect(dbProject.currentState).toEqual(projectBody.currentState);
+    expect(dbProject.date).toEqual(projectBody.date);
+    expect(dbProject.tags).toEqual(projectBody.tags);
+    expect(dbProject.createdAt).toEqual(projectBody.createdAt);
+    expect(dbProject.userId).toEqual(projectBody.userId);
+	});
+  test('delete project', async () => {
+		// Setup for this test specifically.
+		await app.context.db.Project.destroy({ 
+			where: {
+				id: projectId,
+			}
+		 });
+		const deletedProject = await app.context.db.Project.findOne({
+			where: { 
+				 id: projectId,
+			 },
+	 	});
+		expect(deletedProject).toBeNull();
+	});
+});
